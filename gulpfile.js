65428b9819e0bb05c9d8e3384c5dff735c723768
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2014-12-08 23:34:27 +0000

    Front page.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var react = require('gulp-react');
+
+gulp.task('less', function() {
+    return gulp.src(['./source/less/styles.less'])
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('react', function() {
+    return gulp.src(['./source/js/*.js'])
+        .pipe(react({
+        	sourceMap: true
+        }))
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./source/less/**/*.less', ['less']);
+    gulp.watch('./source/js/**/*.js', ['react']);
+});
+
+gulp.task('default', ['less', 'react', 'watch']);
---
+++ source/js/app.js
@@ -0,0 +1,23 @@
+var Application = React.createClass({
+	render: function() {
+		return (
+			<header>
+				<div>
+					<a href="http://github.com/fedosejev"><i className="fa fa-github"></i></a>
+					<a href="http://linkedin.com/in/artemij"><i className="fa fa-linkedin-square"></i></a>
+					<a href="http://medium.com/@artemij"><i className="fa fa-book"></i></a>
+					<a href="mailto:artemij.com@gmail.com"><i className="fa fa-envelope-o"></i></a>
+				</div>
+				<h1>Artemij Fedosejev</h1>
+				<h2>
+					Front End Developer &amp; Entrepreneur
+				</h2>
+			</header>
+		);
+	}
+});
+
+React.render(
+	<Application />,
+	document.querySelectorAll('[data-application-container]')[0]
+);
