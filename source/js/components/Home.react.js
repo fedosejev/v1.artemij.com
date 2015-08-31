82d43772b7ad0e5ed9719a49ab86ae03a1725f8c
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-08-31 01:34:19 +0100

    Update layout

--- source/js/components/Home.react.js
+++ source/js/components/Home.react.js
@@ -17,14 +17,14 @@ var Home = React.createClass({
     return (
       <div className="container" style={containerStyle}>
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-sm-6 col-md-5 col-md-offset-1">
 
             <Photo />
             <Name />
             <Navigation />
 
           </div>
-          <div className="col-sm-6">
+          <div className="col-sm-6 col-md-5">
 
             <Book />
 
